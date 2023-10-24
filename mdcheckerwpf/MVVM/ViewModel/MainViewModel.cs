9cc6948ee326c5cbc8f590e74a0bc7622b40c089
Omkinus <omkinus@gmail.com>
2023-10-24 17:36:52 +0300

    design fixes

---
+++ mdcheckerwpf/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,17 @@
+﻿using mdcheckerwpf.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mdcheckerwpf.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+        public MainViewModel()
+        {
+             
+        }
+    }
+}
