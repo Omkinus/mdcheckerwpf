04af056f982952351dd00f9ffa8fa44028d0d8cc
Omkinus <omkinus@gmail.com>
2024-09-09 18:26:08 +0300

    start page work

--- mdcheckerwpf/obj/Debug/mdcheckerwpf_Content.g.i.cs
+++ mdcheckerwpf/obj/Debug/mdcheckerwpf_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("resources/background2.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("resources/background3.png")]
 
 
